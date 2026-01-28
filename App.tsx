6acdd4e434b4091fa17c42056162c58e0a73fd24
Ha5ko <shantu.hasko@ab-inbev.com>
2026-01-28 22:25:19 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // ========== CONFIGURATION ==========
 // Replace this URL with your Google Apps Script Web App URL
 // See google-apps-script/Code.gs for setup instructions
-const GOOGLE_SCRIPT_URL = 'YOUR_GOOGLE_SCRIPT_URL_HERE';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwmoIvII5rUWeNc_flN5wuLPkufup-_0GZ69jaeEW8aN1YMniVXhKtIZ4js71yu04wHQA/exec';
 const USER_EMAIL = 'shantu.hasko@ab-inbev.com';
 // ===================================
 
